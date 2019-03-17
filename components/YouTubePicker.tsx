bf068133e0eb2ce01d65e667340593896b68444b
Jason Liquorish <jason@liquori.sh>
2019-03-17 20:20:32 +0000

    Initial spike to getting search working

--- components/YouTubePicker.tsx
+++ components/YouTubePicker.tsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import config from "../config.js";
 
 const YouTubePicker = ({ disabled, customElementApi }) => {
   const videoIdRegex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)?([a-zA-Z0-9_-]{11})/;
 
   const [videoId, setVideoId] = useState(null);
+  const [debouncedCallback] = useDebouncedCallback(
+    value => {
+      testSearch(value);
+    },
+    500,
+    []
+  );
+  const [results, setResults] = useState(null);
   const containerRef = useRef(null);
 
   useEffect(() => {
@@ -12,7 +22,10 @@ const YouTubePicker = ({ disabled, customElementApi }) => {
   }, [videoId]);
 
   const handleInput = e => {
-    let matches = videoIdRegex.exec(e.target.value);
+    const searchTerm = e.target.value;
+    // testSearch(searchTerm);
+    debouncedCallback(searchTerm);
+    let matches = videoIdRegex.exec(searchTerm);
 
     if (matches && matches.length > 1) {
       const videoId = matches[1];
@@ -23,22 +36,63 @@ const YouTubePicker = ({ disabled, customElementApi }) => {
     }
   };
 
+  const testSearch = videoId => {
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&type=video&videoEmbeddable=true&q=${videoId}&key=${
+        config.youTubeApiKey
+      }`
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        const json = JSON.stringify(myJson);
+        console.log(myJson);
+        setResults(myJson);
+      });
+  };
+
   return (
     <div ref={containerRef}>
-      <YouTubeInput disabled={disabled} handleInput={handleInput} />
+      <YouTubeInput
+        disabled={disabled}
+        handleInput={handleInput}
+        results={results}
+      />
       <YouTubePreview videoId={videoId} />
     </div>
   );
 };
 
-const YouTubeInput = ({ disabled, handleInput }) => {
+const YouTubeInput = ({ disabled, handleInput, results }) => {
+  console.log(results && results.items);
   return (
-    <input
-      type="text"
-      placeholder="YouTube URL or Video ID"
-      disabled={disabled}
-      onChange={handleInput}
-    />
+    <>
+      <input
+        type="text"
+        placeholder="YouTube URL or Video ID"
+        disabled={disabled}
+        onChange={handleInput}
+      />
+      {results && results.items && (
+        <div className="search-results">
+          {results.items.map(
+            ({
+              id: { videoId },
+              snippet: {
+                title,
+                thumbnails: { default: thumbnail }
+              }
+            }) => (
+              <div key={videoId}>
+                <img src={thumbnail.url} />
+                <span dangerouslySetInnerHTML={{ __html: title }} />
+              </div>
+            )
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
