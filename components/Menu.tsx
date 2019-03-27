a4c489225f22049e2dacbf4548220816ee8847b2
Jason Liquorish <jason@liquori.sh>
2019-03-27 23:19:26 +0000

    Fix box shadow being visible when menu is closed

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -5,7 +5,7 @@ const Menu = styled.div`
   width: 50%;
   max-height: 90vh;
   overflow-y: auto;
-  box-shadow: 0 1px 3px 1px hsla(0, 0%, 0%, 0.2);
+  box-shadow: 0 1px 3px 0 hsla(0, 0%, 0%, 0.2);
 `;
 
 export default Menu;
